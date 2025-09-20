e6623d2af6284876b066cc263c519736e26da74e
marti <marti@DESKTOP-23DSNJ2>
2025-09-20 20:30:33 -0300

    Create Profiles - Artist and Music

---
+++ MusicSoundAPI/MusicSoundAPI/Profiles/ArtistProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MusicSoundAPI.Data.Artista;
+using MusicSoundAPI.Models;
+
+namespace MusicSoundAPI.Profiles
+{
+    public class ArtistProfile : Profile
+    {
+        public ArtistProfile()
+        {
+            CreateMap<Artista, CreateArtistDTO>();
+            CreateMap<CreateArtistDTO, Artista>();
+            CreateMap<ReadArtistDTO, Artista>();
+            CreateMap<Artista, ReadArtistDTO>();
+        }
+    }
+}
---
+++ MusicSoundAPI/MusicSoundAPI/Profiles/MusicProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using MusicSoundAPI.Data.Musica;
+using MusicSoundAPI.Models;
+
+namespace MusicSoundAPI.Profiles
+{
+    public class MusicProfile : Profile
+    {
+        public MusicProfile()
+        {
+            CreateMap<Musica, CreateMusicDTO>();
+            CreateMap<CreateMusicDTO, Musica>();
+            CreateMap<ReadMusicDTO, Musica>();
+            CreateMap<Musica, ReadMusicDTO>();
+        }
+    }
+}
