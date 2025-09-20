d7514a210f5c4ad0d089160441899a37b0d6b5ad
marti <marti@DESKTOP-23DSNJ2>
2025-09-20 20:31:01 -0300

    Settings of MusicSoundAPI

--- MusicSoundAPI/MusicSoundAPI/Program.cs
+++ MusicSoundAPI/MusicSoundAPI/Program.cs
@@ -1,6 +1,29 @@
+using Microsoft.EntityFrameworkCore;
+using MusicSoundAPI.ApplicationDbContext;
+using MusicSoundAPI.Repositories.Artist;
+using MusicSoundAPI.Repositories.Music;
+using MusicSoundAPI.Services.Artist;
+using MusicSoundAPI.Services.Music;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+// Adding Oracle DB Context
+var connString = builder.Configuration.GetConnectionString("DEV");
+builder.Services.AddDbContext<ModelContext>(options =>
+    options.UseOracle(connString));
+
+// Adding Profile
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+// Adding Repositories
+builder.Services.AddScoped<IArtistRepository, ArtistRepository>();
+builder.Services.AddScoped<IMusicRepository, MusicRepository>();
+
+// Adding Services
+builder.Services.AddScoped<IArtistService, ArtistService>();
+builder.Services.AddScoped<IMusicService, MusicService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
