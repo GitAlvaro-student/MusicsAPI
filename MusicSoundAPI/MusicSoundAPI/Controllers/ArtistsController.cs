8b5b954284e0037750741c7e69de38c196b9171a
marti <marti@DESKTOP-23DSNJ2>
2025-09-20 20:28:47 -0300

    Create Controllers - Artist and Music

---
+++ MusicSoundAPI/MusicSoundAPI/Controllers/ArtistsController.cs
@@ -0,0 +1,111 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicSoundAPI.Data.Artista;
+using MusicSoundAPI.Models;
+using MusicSoundAPI.Services.Artist;
+using Serilog;
+
+namespace MusicSoundAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ArtistsController : ControllerBase
+    {
+        private readonly IArtistService _artistService;
+        //private readonly ILogger<ArtistController> _logger;
+
+        public ArtistsController(IArtistService artistService/*, ILogger<ArtistaController> logger*/)
+        {
+            _artistService = artistService;
+            //_logger = logger;
+        }
+
+        /// <summary>
+        /// Mostra informações de um Artista de acordo com o Id
+        /// </summary>
+        /// <param name="idArtist">Id do Artista a ser Mostrado</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
+        [HttpGet("{idArtist}")]
+        public async Task<IActionResult> GetArtistById(int idArtist)
+        {
+            var artist = await _artistService.GetArtistaById(idArtist);
+            //_logger.LogInformation($"Buscando Artista pelo Id {idArtist}");
+
+            if (artist == null)
+            {
+                //_logger.LogInformation($"Artista de Id {idArtist} nâo encontrado");
+                //var nullLog = SetLog(LogLevel.Information.ToString(), $"Artista não encontrado", "GetArtistById", idArtist);
+                //Log.Information(nullLog);
+
+                return NotFound("Artista Não Encontrado!!");
+            }
+
+            //var log = SetLog(LogLevel.Information.ToString(), "Sucesso!", "GetArtistById", idArtist);
+            //Log.Information(log);
+
+            return Ok(artist);
+        }
+
+        /// <summary>
+        /// Mostra informações de uma lista de Artistas
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
+        [HttpGet]
+        public async Task<IActionResult> GetAllArtists()
+        {
+            var artist = await _artistService.GetAllArtistas();
+            var artistCount = artist.Count();
+
+            if (artist == null)
+            {
+                //var nullLog = SetLog(LogLevel.Information.ToString(), $"Artistas não encontrados", "GetAllArtists");
+                //Log.Information(nullLog);
+
+                return NotFound("Artistas Não Encontrados!!");
+            }
+
+            //var log = SetLog(LogLevel.Information.ToString(), "Sucesso!", "GetAllArtists");
+            //Log.Information(log);
+
+            return Ok(new
+            {
+                Mensagem = $"{artistCount} Artistas Registrados",
+                Dados = artist.OrderBy(m => m.Nome).Take(10)
+            });
+        }
+
+        /// <summary>
+        /// Insere informações de um Artista no Banco de Dados
+        /// </summary>
+        /// <param name="artist">Objeto necessário para inserir uma Artista</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
+        [Route("RegisterArtist")]
+        [HttpPost]
+        public async Task<ActionResult<Artista>> PostArtist(CreateArtistDTO artist)
+        {
+            //_logger.LogInformation($"Executando Método {HttpContext.Request.PathBase}");
+            try
+            {
+                if (!ModelState.IsValid)
+                {
+                    /*_logger.LogError($"Dados Inválidos Enviados" +
+                        $"{artist}");*/
+                    return BadRequest(ModelState);
+                }
+
+                await _artistService.InsertArtista(artist);
+                return CreatedAtAction(nameof(GetAllArtists), new { name = artist.Nome }, artist);
+
+            }
+            catch (Exception ex)
+            {
+                //_logger.LogError($"O seguinte erro ocorreu {ex.Message}");
+                return BadRequest(ex.Message);
+            }
+
+        }
+    }
+}
---
+++ MusicSoundAPI/MusicSoundAPI/Controllers/MusicsController.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MusicSoundAPI.Data.Musica;
+using MusicSoundAPI.Services.Music;
+using Serilog;
+
+namespace MusicSoundAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MusicsController : ControllerBase
+    {
+        private readonly IMusicService _musicService;
+
+
+        public MusicsController(IMusicService musicService)
+        {
+            _musicService = musicService;
+        }
+
+        /// <summary>
+        /// Mostra informações de uma Música de acordo com o Id
+        /// </summary>
+        /// <param name="idMusic">Id da Música a ser Mostrada</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
+        [HttpGet("byMusicId")]
+        public async Task<IActionResult> GetMusicById(int idMusic)
+        {
+            var song = await _musicService.GetMusicById(idMusic);
+
+            /*var log = SetLog(LogLevel.Information.ToString(), "Sucesso!", "GetMusicById", idMusic);
+            Log.Information(log);*/
+
+            return Ok(song);
+        }
+
+        /// <summary>
+        /// Mostra informações de uma lista de Músicas
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        /// <response code="200">Caso a requisição seja feita com sucesso</response>
+        [HttpGet("AllMusics")]
+        public async Task<IActionResult> GetAllMusics()
+        {
+            var song = await _musicService.GetMusics();
+
+            /*var log = SetLog(LogLevel.Information.ToString(), "Sucesso!", "GetAllMusics");
+            Log.Information(log);*/
+
+            return Ok(song);
+        }
+
+        /// <summary>
+        /// Insere informações de uma Música no Banco de Dados
+        /// </summary>
+        /// <param name="music">Objeto necessário para inserir uma Música</param>
+        /// <returns>IActionResult</returns>
+        /// <response code="201">Caso inserção seja feita com sucesso</response>
+        [HttpPost("RegisterMusic")]
+        public async Task<IActionResult> PostMusic(CreateMusicDTO music)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            await _musicService.InsertMusic(music);
+
+            /*var log = SetLog(LogLevel.Information.ToString(), "Sucesso!", "PostMusic", music);
+            Log.Information(log);*/
+
+            return CreatedAtAction(nameof(PostMusic), music);
+
+        }
+    }
+}
